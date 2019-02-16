ded1015b65e4f3a02951f2c7590b404dfb617c4f
Cerjjapan <cerjjapan@yahoo.com>
2019-02-16 11:50:18 +0000

    latest product model and route and app.js

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ const express = require('express'),
       LocalStrategy = require('passport-local'),
       methodOverride = require('method-override'),
       Product = require('./models/product'),
-      Comment = require('./models/comment'),
       User = require('./models/user');
 // seedDB          = require("./seeds")
 
 //Requiring Routes
-const loginRoutes = require('./routes/login');
+
 const productRoutes = require('./routes/product');
-const commentRoutes = require('./routes/comments');
+
 
 app.get('/', (req, res) => {
     res.send({ 
@@ -31,16 +30,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 
-// app.locals.moment = require('moment');
 
-//  PASSPORT CONFIGURATION
-// app.use(
-//     require('express-session')({
-//         secret: 'Blade Runner is the best movie!',
-//         resave: false,
-//         saveUninitialized: false
-//     })
-// );
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -50,9 +40,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// app.use("/", indexRoutes);
 app.use("/product", productRoutes);
-app.use("/comments", commentRoutes);
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.warn(
--- models/comment.js
+++ models/comment.js
@@ -4,13 +4,13 @@ const commentSchema = new mongoose.Schema({
     productID: String,
     text: String,
     createdAt: { type: Date, default: Date.now },
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
-    }
+    // author: {
+    //     id: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'User'
+    //     },
+    //     username: String
+    // }
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
