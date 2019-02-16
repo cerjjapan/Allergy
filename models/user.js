7e91daf95c31fdf22d2dec11731da934fe94949f
Robert Bausmert <robert@Roberts-MacBook-Air.local>
2019-02-16 19:03:48 +0900

    fixed conflict

--- models/user.js
+++ models/user.js
@@ -2,15 +2,9 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
   username: { type: String, required: true },
   password: { type: String, required: true },
   allergies: { type: Array, default: [] }
-=======
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    allergies: { type: Array, default: [] }
->>>>>>> 4ab9f05e3630b44e13145b0fcb0e78f48f98ee59
 });
 
 UserSchema.plugin(passportLocalMongoose);
