8207937a31d6a33ca89dea05ddc0e631d092d029
Sergei Heymann <cerjjapan@Sergeis-MacBook-Pro.local>
2019-02-16 10:56:13 +0900

    i did stuff

--- models/product.js
+++ models/product.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  productname: String
-});
+            productName: { type: String, required: true },
+            image: { type: String, required: true },
+            comment: {
+                id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Comment'
+                },
+            });
 
-ProductSchema.plugin(passportLocalMongoose);
+        ProductSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('Product', ProductSchema);
+        module.exports = mongoose.model('Product', ProductSchema);
